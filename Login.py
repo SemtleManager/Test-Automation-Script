5be58fa44a88dd699c4a36bdccdc0e5a4011bbc9
Leeseonmin <tjsalsrns96@naver.com>
2023-02-02 01:05:42 +0900

    update scenario

--- Login.py
+++ Login.py
@@ -9,19 +9,23 @@ from selenium.webdriver.chrome.service import Service
 import openpyxl
 from datetime import datetime
 
-scene_list = ['정상 시나리오', 'ID 입력 오류', 'PW 입력 오류']  # 시나리오 리스트
-inform_list = ['단계', '시나리오', '날짜', '합격 여부', '오류 코드']  # 각 시나리오에 대한 테스트케이스 수행 정보
+scene_list = ['정상 시나리오', 'ID 입력 오류', 'PW 입력 오류','존재 하지 않는 계정 로그인']  # 시나리오 리스트
+inform_list = ['단계', '테스트 케이스', '입력 값','기대 값','날짜', '합격 여부', '오류 코드']  # 각 시나리오에 대한 테스트케이스 수행 정보
 exl_file = openpyxl.Workbook()  # 엑셀 보고서 작성
 exl_sheet = exl_file.active
 exl_sheet.append(inform_list)
 exl_sheet.column_dimensions['B'].width = 20
 exl_sheet.column_dimensions['C'].width = 40
-exl_sheet.column_dimensions['E'].width = 105
+exl_sheet.column_dimensions['D'].width = 40
+exl_sheet.column_dimensions['E'].width = 20
+exl_sheet.column_dimensions['G'].width = 105
 
 
 def login_normal(scene: int):  # 구분 값
     now = datetime.now()
-    inform = [scene, scene_list[scene], now, str(False)]  # 테스트 수행 정보
+    input_data = ['201500011', 't1234']
+    expected_val = '로그인 성공'
+    inform = [scene, scene_list[scene], str(input_data), expected_val, now, str(False)]  # 테스트 수행 정보
     chromedriver = 'C:\devpython\Webdriver\chromedriver.exe'
     tester = webdriver.Chrome(service=Service(chromedriver))  # 브라우저 : Chrome
 
@@ -59,7 +63,9 @@ def login_normal(scene: int):  # 구분 값
 
 def login_emptyID(scene: int):  # 구분 값
     now = datetime.now()
-    inform = [scene, scene_list[scene], now, str(False)]  # 테스트 수행 정보
+    input_data=['','t1234']
+    expected_val='로그인 실패 알림창 등장'
+    inform = [scene, scene_list[scene],str(input_data),expected_val, now, str(False)]  # 테스트 수행 정보
     chromedriver = 'C:\devpython\Webdriver\chromedriver.exe'
     tester = webdriver.Chrome(service=Service(chromedriver))  # 브라우저 : Chrome
 
@@ -104,7 +110,9 @@ def login_emptyID(scene: int):  # 구분 값
 
 def login_emptyPW(scene: int):  # 구분 값
     now = datetime.now()
-    inform = [scene, scene_list[scene], now, str(False)]  # 테스트 수행 정보
+    input_data = ['201500011', '']
+    expected_val = '로그인 실패 알림창 등장'
+    inform = [scene, scene_list[scene], str(input_data), expected_val, now, str(False)]  # 테스트 수행 정보
     chromedriver = 'C:\devpython\Webdriver\chromedriver.exe'
     tester = webdriver.Chrome(service=Service(chromedriver))  # 브라우저 : Chrome
 
@@ -148,6 +156,53 @@ def login_emptyPW(scene: int):  # 구분 값
     scene_info_save(exl_sheet, tester, inform) #시나리오 저장
 
 
+def login_emptyAccount(scene: int):  # 구분 값
+    now = datetime.now()
+    input_data = ['NoAccount', 'NoAccount']
+    expected_val = '로그인 실패 알림창 등장'
+    inform = [scene, scene_list[scene], str(input_data), expected_val, now, str(False)]  # 테스트 수행 정보
+    chromedriver = 'C:\devpython\Webdriver\chromedriver.exe'
+    tester = webdriver.Chrome(service=Service(chromedriver))  # 브라우저 : Chrome
+
+    try:
+        tester.get('http://semtle.catholic.ac.kr')  # 해당 사이트 테스트
+
+        tester.maximize_window()  # 화면 크기 최대
+        tester.implicitly_wait(10)
+
+        login_btn = tester.find_element(By.XPATH, "//div[@class='member-header']/h3/a")  # 로그인 버튼
+        login_btn.click()
+
+        tester.implicitly_wait(10)
+        time.sleep(1)
+        id_input = tester.find_element(By.CSS_SELECTOR, '#loginform > div.idform > input')
+        pw_input = tester.find_element(By.CSS_SELECTOR, '#loginform > div.pwform > input')
+        rlogin_btn = tester.find_element(By.CSS_SELECTOR, '#loginform > div:nth-child(5) > input')
+
+        id_input.send_keys('NoAccount')
+        pw_input.send_keys('NoAccount')
+        time.sleep(1)
+
+        rlogin_btn.click()
+        try:
+            alert = tester.switch_to.alert
+            time.sleep(1)
+            alert.accept()
+        except Exception as e:
+            inform.append(e)
+            scene_info_save(exl_sheet, tester, inform)
+            return
+
+        tester.implicitly_wait(10)
+        print(tester.current_url)
+        time.sleep(2)
+        inform[-1] = str(True)
+
+    except Exception as e:
+        inform.append(e)
+
+    scene_info_save(exl_sheet, tester, inform) #시나리오 저장
+
 def scene_info_save(sheet, tester, info):
     sheet.append(info)
     tester.quit()
@@ -160,10 +215,9 @@ def create_report():
     print('complete!')
 
 
-testcases = [login_normal, login_emptyID, login_emptyPW]
+testcases = [login_normal, login_emptyID, login_emptyPW,login_emptyAccount]
 
 for num, case in enumerate(testcases):
     case(num)
-    time.sleep(1)
 
 create_report()
--- SignUp.py
+++ SignUp.py
@@ -9,21 +9,28 @@ from selenium.webdriver.chrome.service import Service
 import openpyxl
 from datetime import datetime
 
-scene_list=['정상 시나리오','아이디 입력란 공백','PW 입력란 공백'] #시나리오 리스트
-inform_list=['단계','시나리오','날짜','합격 여부','오류 코드'] #각 시나리오에 대한 테스트케이스 수행 정보
+scene_list=['정상 시나리오','아이디 입력란 공백','PW 입력란 공백','PW 확인란 공백','닉네임 공백','이름 공백','학번 공백','이메일 공백','핸드폰 번호 공백'] #시나리오 리스트
+inform_list=['단계','시나리오','입력 값','기대 값','날짜','합격 여부','오류 코드'] #각 시나리오에 대한 테스트케이스 수행 정보
+test_data=['201500011','t1234','t1234','Gorani','HongGilDong','201500011','test@naver.com','01012341234']
 exl_file = openpyxl.Workbook()  # 엑셀 보고서 작성
 exl_sheet = exl_file.active
 exl_sheet.append(inform_list)
 exl_sheet.column_dimensions['B'].width=20
-exl_sheet.column_dimensions['C'].width=40
-exl_sheet.column_dimensions['E'].width=105
+exl_sheet.column_dimensions['C'].width=80
+exl_sheet.column_dimensions['D'].width=40
+exl_sheet.column_dimensions['E'].width=25
+exl_sheet.column_dimensions['F'].width=20
+exl_sheet.column_dimensions['G'].width=100
 
-chromedriver='C:\devpython\Webdriver\chromedriver.exe'
-tester = webdriver.Chrome(service=Service(chromedriver)) #브라우저 : Chrome
+
 
 def signup_normal(scene : int): #구분 값
     now = datetime.now()
-    inform=[scene,scene_list[scene],now,str(False)] #테스트 수행 정보
+    expected_val = '회원가입 완료 알림창 등장'
+    inform = [scene, scene_list[scene], str(test_data), expected_val, now, str(False)]  # 테스트 수행 정보
+
+    chromedriver = 'C:\devpython\Webdriver\chromedriver.exe'
+    tester = webdriver.Chrome(service=Service(chromedriver))  # 브라우저 : Chrome
 
     try:
         tester.get('http://semtle.catholic.ac.kr') #해당 사이트 테스트
@@ -40,21 +47,77 @@ def signup_normal(scene : int): #구분 값
         tester.implicitly_wait(10)
 
         #데이터 입력
-        test_data=['201500011','t1234','t1234','Gorani','HongGilDong','201500011','test@naver.com','01012341234']
         inputs=tester.find_elements(By.CSS_SELECTOR,'div.join > form > div.join_input > input')
         dropdown=Select(tester.find_element(By.CSS_SELECTOR,'div.join > form > div.join_input > select'))
 
         for idx,input_box in enumerate(inputs):
             input_box.send_keys(test_data[idx])
-            time.sleep(2)
         dropdown.select_by_value('4')
-        time.sleep(2)
 
         tester.execute_script('window.scrollTo(0,document.body.scrollHeight)')
 
         chkbox=tester.find_element(By.CSS_SELECTOR,'#marketing')
         chkbox.click()
         time.sleep(2)
+
+        rsign_btn=tester.find_element(By.CSS_SELECTOR,'#sign > div.join_input.join_btnbox > button')
+        rsign_btn.click()
+
+        try:
+            alert_box=tester.switch_to.alert
+            time.sleep(1)
+            alert_box.accept()
+        except:
+            pass
+
+        tester.implicitly_wait(10)
+        time.sleep(2)
+
+        inform[-1]=str(True) #
+
+    except Exception as e:
+        inform.append(e)
+
+    exl_sheet.append(inform) #해당 시나리오 수행 정보 저장
+    tester.quit()
+
+def signup_blank(scene : int, blank : int): #구분 값
+    now = datetime.now()
+    input_data = test_data[:]
+    input_data[blank] = ''
+    expected_val='회원가입 실패 알림 창 등장'
+    inform=[scene,scene_list[scene],str(input_data),expected_val,now,str(False)] #테스트 수행 정보
+
+    chromedriver = 'C:\devpython\Webdriver\chromedriver.exe'
+    tester = webdriver.Chrome(service=Service(chromedriver))  # 브라우저 : Chrome
+
+    try:
+        tester.get('http://semtle.catholic.ac.kr') #해당 사이트 테스트
+
+        tester.maximize_window() #화면 크기 최대
+        tester.implicitly_wait(10)
+
+        login_btn=tester.find_element(By.XPATH,"//div[@class='member-header']/h3/a") #로그인 버튼
+        login_btn.click()
+        tester.implicitly_wait(10)
+
+        signup_btn=tester.find_element(By.CSS_SELECTOR,'#loginform > div.signupform > a')
+        signup_btn.click()
+        tester.implicitly_wait(10)
+
+        #데이터 입력
+        inputs=tester.find_elements(By.CSS_SELECTOR,'div.join > form > div.join_input > input')
+        dropdown=Select(tester.find_element(By.CSS_SELECTOR,'div.join > form > div.join_input > select'))
+
+        for idx,input_box in enumerate(inputs):
+            input_box.send_keys(input_data[idx])
+        dropdown.select_by_value('4')
+
+        tester.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+
+        chkbox=tester.find_element(By.CSS_SELECTOR,'#marketing')
+        chkbox.click()
+        time.sleep(1)
 
         rsign_btn=tester.find_element(By.CSS_SELECTOR,'#sign > div.join_input.join_btnbox > button')
         rsign_btn.click()
@@ -85,4 +148,6 @@ def create_report():
 
 
 signup_normal(0)
+for i in range(1,9):
+    signup_blank(i,i-1)
 create_report()
